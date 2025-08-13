a0e2a51781f5c60a4ec166ec087091adffa2670f
fredery-munoz <fredery.munoz@uao.edu.co>
2025-08-12 19:31:58 -0500

    aprendiendo a subir

---
+++ funcion-flecha.js
@@ -0,0 +1,14 @@
+function nombredelafuncion(n1,n2){
+    console.log ("los numeros son :", n1, n2)
+}
+
+const variable =()=>{//otra forma de crear una funcion 
+
+}
+
+const imprimirnombre =(nombre)=>{
+    return "holita" + nombre;
+}
+
+resultado = imprimirnombre ("fredery")
+console.log(resultado)
