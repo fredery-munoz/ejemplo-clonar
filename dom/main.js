fb535525fd4994e435642e2cbb812f8a7140dcad
fredery-munoz <fredery.munoz@uao.edu.co>
2025-08-12 20:52:29 -0500

    haciendo objetos desde js

---
+++ dom/main.js
@@ -0,0 +1,28 @@
+const v1 = "veni para aka ole";
+const v2 = "por que o si no mejor dicho";
+const v3 = "yo no quiero ser spider man si no wolvering";
+const v4 = "este no se para que pero bueno";
+
+const batman = (peligro) =>{
+    if (peligro == true){
+        return `como te decia ${v1} ${v2}`;
+    }else{
+        return `como te decia ${v3} ${v4}`;
+    }
+}
+
+// para crear un titulo
+const titulo = document.createElement("h2");
+titulo.textContent ="hola me llamo batman";
+document.body.appendChild(titulo);  //para colocarlo en la pantalla html
+
+// para crear un boton
+const boton = document.createElement("button");
+const boton2 = document.createElement("button");
+
+boton.innerText = "peligro";
+boton2.innerText = "no peligro";
+
+document.body.appendChild(boton);
+document.body.appendChild(boton2);
+
